31acffab93e96d942bcb96bb01d4cf969f4c5214
Gerald Manipon <pymonger@gmail.com>
2020-06-10 15:44:01 +0000

    add script to run test

---
+++ run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}"; pwd)
+
+# source ISCE2 environment
+source /opt/isce2/isce_env.sh
+
+# export ISCE2 applications path
+PATH=/opt/isce2/isce/applications:$PATH
+
+# clean out previous run
+rm -rf ESD PICKLE coarse_* fine_* geom_* isce.log master merged slave topsProc.xml topsinsar.log
+
+# run
+topsApp.py --steps --end=geocode
